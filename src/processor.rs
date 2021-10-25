37ce27913e9bbb9f6ad9888035d7b72f84634e09
smaster0517 <smaster0517@gmail.com>
2021-10-25 16:42:31 +0300

    refactored

--- src/processor.rs
+++ src/processor.rs
@@ -1,33 +1,28 @@
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
+    program_error::ProgramError,
     msg,
     pubkey::Pubkey,
     program_pack::{Pack, IsInitialized},
-    sysvar::{rent::Rent, Sysvar},
-    program::invoke
+    sysvar::{rent::Rent, Sysvar}
 };
 
-use crate::{ 
-    instruction::EscrowInstruction,
-    error::EscrowError,
-    state::Escrow
-};
+use crate::{ instruction::EscrowInstruction, error::EscrowError, state::Escrow };
 
 pub struct Processor;
-
 impl Processor {
-    pub fn process(program_id: &Pubkey, account: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
-        let instruction = EscrowInstruction::unpack(instruction_data)?;
-
+    pub fn process(program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
+        let instruction = EscrowInstruction::unpack(instruction_data);
         match instruction {
             EscrowInstruction::InitEscrow { amount } => {
                 msg!("Instruction: InitEscrow");
-                Self::process_init_escrow(account, amount, program_id)
+                Self::process_init_escrow(accounts, amount, program_id)
             }
         }
     }
-    fn process_init_escrow(
+
+    fn process_init_escrow (
         accounts: &[AccountInfo],
         amount: u64,
         program_id: &Pubkey,
@@ -61,22 +56,11 @@ impl Processor {
         escrow_info.is_initialized = true;
         escrow_info.initializer_pubkey = *initializer.key;
         escrow_info.temp_token_account_pubkey = *temp_token_account.key;
-        escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
+        escrow_info.initializer_token_to_receive_account_pubkey = 
         escrow_info.expected_amount = amount;
 
-        Escrow::pack(escrow_info, &mut escrow_account.data.borrow_mut())?;
-        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
-
-        let token_program = next_account_info(account_info_iter)?;
-        let owner_change_ix = spl_token::instruction::set_authority(
-            token_program.key,
-            temp_token_account.key,
-            Some(&pda),
-            spl_token::instruction::AuthorityType::AccountOwner,
-            initializer.key,
-            &[&initializer.key],
-        )?;
-        
+        Escrow::pack(escrow_info, &mut escrow_account_)
         Ok(())
     }
 }
+
